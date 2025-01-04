344d72faadcf4765494367c407f95dc1d68c8c97
ss-leg-al <hyunseo4242@gmail.com>
2025-01-04 14:44:06 +0900

    login without server

--- App.js
+++ App.js
@@ -8,13 +8,16 @@ const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const chatbotRoutes = require('./routes/chatbotRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 // Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/login', authRoutes);
 app.use('/chat', chatbotRoutes);
 
 
