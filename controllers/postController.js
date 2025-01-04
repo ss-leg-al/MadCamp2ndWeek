804ac091ae7bbc13dd476b31ec65b173d5d42ef7
ss-leg-al <hyunseo4242@gmail.com>
2025-01-04 10:30:13 +0900

    add searching

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,6 +30,32 @@ const postController = {
       res.status(500).json({ error: err.message });
     }
   },
+  searchPosts: async (req, res) => {
+    try {
+      const { query } = req.query; // 쿼리 파라미터에서 검색어 가져오기
+
+      if (!query) {
+        return res.status(400).json({ error: 'Query parameter is required' });
+      }
+
+      // 제목 또는 내용에서 검색어 찾기
+      const posts = await Post.find({
+        $or: [
+          { title: { $regex: query, $options: 'i' } }, // 대소문자 구분 없이 검색
+          { content: { $regex: query, $options: 'i' } },
+        ],
+      });
+      if (posts.length === 0) {
+        return res.status(200).json({ message: 'No posts found', posts: [] });
+      }
+      res.status(200).json(posts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
 };
+
 
 module.exports = postController;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+router.get('/search',postController.searchPosts);
+router.get('/:id', postController.getPost);
 router.post('/', postController.createPost);
 router.get('/', postController.getPosts);
-router.get('/:id', postController.getPost);
+
+
 
 module.exports = router;
